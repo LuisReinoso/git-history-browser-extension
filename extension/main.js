f3793ee7f7673abdbcec77919de2c108c44e157e
Luis Reinoso <lomejordejr@gmail.com>
2019-02-09 12:26:10 -0500

    Cambio en el nombre del proyecto y la url a la que redirige a github.githistory.xyz

--- extension/main.js
+++ extension/main.js
@@ -1,10 +1,10 @@
 let url = chrome ? document.URL : window.location.href;
 const auxUrl = url.split('/');
-auxUrl[2] = 'github-history.netlify.com'
+auxUrl[2] = 'github.githistory.xyz'
 url = auxUrl.join('/');
 
 const buttonGithubHistory = document.createElement('a');
-buttonGithubHistory.innerHTML = 'File history';
+buttonGithubHistory.innerHTML = 'Open in Git History';
 buttonGithubHistory.setAttribute('class', 'btn btn-sm BtnGroup-item');
 buttonGithubHistory.setAttribute('href', url);
 document.getElementById("raw-url").parentNode.appendChild(buttonGithubHistory);
