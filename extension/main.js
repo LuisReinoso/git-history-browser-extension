e7ead58a366d225f4f72636a11f4575e0e9ee7de
Luis Reinoso <lomejordejr@gmail.com>
2019-02-24 10:59:19 -0500

    Agregado git history button a gitlab.com. #1

--- extension/main.js
+++ extension/main.js
@@ -1,28 +1,66 @@
-this.isButtonInserted(document.URL);
-let oldUrl = document.URL;
-// It's necessary because git use pjax to refresh views
-// https://stackoverflow.com/questions/3522090/event-when-window-location-href-changes#46428962
-window.onload = () => {
-  let bodyList = document.querySelector('body');
-  const observer = new MutationObserver(mutations => {
-    mutations.forEach(mutation => {
-      if (oldUrl != document.URL) {
-        if (isButtonInserted(document.URL)) {
-        oldUrl = document.URL;
-        }
-      }
-    });
-  });
+const urlProvider = document.URL.split('/');
+switch (urlProvider[2]) {
+  case 'github.com':
+    this.isButtonInsertedGithub(document.URL);
+    let oldUrl = document.URL;
+    // It's necessary because git use pjax to refresh views
+    // https://stackoverflow.com/questions/3522090/event-when-window-location-href-changes#46428962
+    window.onload = () => {
+      let bodyList = document.querySelector('body');
+      const observer = new MutationObserver(mutations => {
+        mutations.forEach(mutation => {
+          if (oldUrl != document.URL) {
+            if (isButtonInsertedGithub(document.URL)) {
+              oldUrl = document.URL;
+            }
+          }
+        });
+      });
 
-  let config = {
-    childList: true,
-    subtree: true
-  };
-  observer.observe(bodyList, config);
-};
+      let config = {
+        childList: true,
+        subtree: true
+      };
+      observer.observe(bodyList, config);
+    };
+    break;
+  case 'bitbucket.org':
+    break;
+  case 'gitlab.com':
+    this.isButtonInsertedGitlab(document.URL);
+    break;
+  default:
+    break;
+}
 
-function isButtonInserted(url) {
-  if (/https:\/\/github\.com\/[a-zA-Z0-9-_]*\/[a-zA-Z0-9-_]*\/blob\/.*/.test(url)) {
+function isButtonInsertedGitlab(url) {
+  if (
+    /https:\/\/gitlab\.com\/[a-zA-Z0-9-_\.]*\/[a-zA-Z0-9-_\.]*\/blob\/.*/.test(url)
+  ) {
+    const auxUrl = url.split('/');
+    auxUrl[2] = 'gitlab.githistory.xyz';
+    url = auxUrl.join('/');
+    const buttonGitHistory = document.createElement('a');
+    buttonGitHistory.innerHTML = 'Open in Git History';
+    buttonGitHistory.setAttribute('class', 'btn btn-default btn-sm');
+    buttonGitHistory.setAttribute('href', url);
+    try {
+      document
+        .getElementsByClassName('file-actions')[0]
+        .childNodes[3].appendChild(buttonGitHistory);
+      return true;
+    } catch (error) {
+      return false;
+    }
+  } else {
+    return false;
+  }
+}
+
+function isButtonInsertedGithub(url) {
+  if (
+    /https:\/\/github\.com\/[a-zA-Z0-9-_\.]*\/[a-zA-Z0-9-_\.]*\/blob\/.*/.test(url)
+  ) {
     const auxUrl = url.split('/');
     auxUrl[2] = 'github.githistory.xyz';
     url = auxUrl.join('/');
